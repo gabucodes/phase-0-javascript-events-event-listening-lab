16a849a0d3ba7aac4d7f2cb071cd82891ee426c1
Gabriel <kipkoechgabriel27@gmail.com>
2025-03-03 08:26:37 +0300

    completes

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+    // Select the button element
+    const button = document.getElementById('button');
+
+    // Add an event listener for "click"
+    button.addEventListener("click", function() {
+        alert("Button was clicked!");
+    });
 }
