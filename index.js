9699694396093a4c0fb46754baa2baf99267db1c
Gabriel <kipkoechgabriel27@gmail.com>
2025-03-07 19:33:35 +0300

    completes phase

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function addingEventListener() {
+//function addingEventListener() {
     // Select the button element
     const button = document.getElementById('button');
 
@@ -6,4 +6,14 @@ function addingEventListener() {
     button.addEventListener("click", function() {
         alert("Button was clicked!");
     });
-}
+//}
+
+function addingEventListener() {
+    const button = document.getElementById('button');
+    if (button) {
+      button.addEventListener('click', () => {
+        console.log('Button clicked!');
+      });
+    }
+  }
+  
